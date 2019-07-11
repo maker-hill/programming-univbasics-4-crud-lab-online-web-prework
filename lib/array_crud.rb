543394dbea615601e8fce41a82e8d5483cbd70d5
Jeremy Hill <j.c.hillmail@gmail.com>
2019-07-11 11:10:45 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,21 +1,25 @@
 def create_an_empty_array
-  
+  create_an_empty_array = []
 end
 
 def create_an_array
-  
+  create_an_array = {love, joy, peace, kindness]
+ p create_an_array.size
 end
 
 def add_element_to_end_of_array(array, element)
-  
+      array = ["wow", "I", "am", "really", "learning"]
+      array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  start_of_array = ["I", "am", "really", "learning"]
+  start_of_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+      array_remove = ["I", "am", "really", "learning", "arrays!"]
+      last_element = array_remove.pop
 end
 
 def remove_element_from_start_of_array(array)
