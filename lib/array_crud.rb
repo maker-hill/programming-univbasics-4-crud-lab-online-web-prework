960b153cd0042f3a648f88f2bcc4c34b0f17c06f
Jeremy Hill <j.c.hillmail@gmail.com>
2019-07-11 11:06:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   start_of_array = ["I", "am", "really", "learning"]
-  
+  start_of_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
